c9afb4205df69cbb77fcfd64f9247f0a145610dd
lrth06 <lrth06@gmail.com>
2020-12-18 18:34:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,6 @@ require("dotenv").config();
 const db = require("./backend/Configuration/db");
 const chalk = require("chalk");
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
-
 //Handle CORS
 app.use(cors());
 
