50b094fdabf2d6aaa3ece2e1e29e9f12e22cfa92
lrth06 <lrth06@gmail.com>
2020-12-18 21:49:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const db = require("./backend/Configuration/db");
 const chalk = require("chalk");
 
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 //Handle CORS
 app.use(cors());
 
