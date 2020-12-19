4028ef03ed4b743d60fa00cf4468d838f01ab42f
lrth06 <lrth06@gmail.com>
2020-12-18 22:08:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const db = require("./backend/Configuration/db");
 const chalk = require("chalk");
 
 app.use("/", express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+});
 //Handle CORS
 app.use(cors());
 
